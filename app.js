99c93fc740ef8c5c623a6ac26b557c9906c08f7c
Raveendra Mokka <raveendra.mokka@stits.co>
2021-04-18 16:43:29 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+app.get('/',(req,res)=>{
+    res.status(200).json('Application is running')
+})
+app.listen(PORT, console.log(`Listening on port ${PORT}`))
